e74286a9f0e28c4d577f12622594eaf827bc690b
programmeramandeep <programmeramandeep@gmail.com>
2021-05-09 20:27:32 +0530

    blog page completed

--- src/pages/BlogsPage.js
+++ src/pages/BlogsPage.js
@@ -1,10 +1,29 @@
 import React from "react";
+import blogs from "../components/allBlogs";
+import Title from "../components/Title";
 
 const BlogsPage = () => {
     return (
-        <div className="BlogsPage">
-            <h1>Blogs</h1>
-        </div>
+        <>
+            <div className="b-title">
+                <Title title={"Recent Blogs"} span={"Recent Blogs"} />
+            </div>
+
+            <div className="BlogsPage">
+                {blogs.map((blog) => {
+                    return (
+                        <div className="blog" key={blog.id}>
+                            <div className="blog-content">
+                                <img src={blog.image} alt="" />
+                                <a href={blog.link} className="blog-link">
+                                    {blog.title}
+                                </a>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
     );
 };
 
